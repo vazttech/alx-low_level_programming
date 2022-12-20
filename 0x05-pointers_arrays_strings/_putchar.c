3c87207a0b7b4847a46cac8f18796e5ff6826a1f
vazttech <michaelukandu30@gmail.com>
2022-12-20 15:55:24 -0800

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar -writes the character c  to stdou
+ * gc: the character to pring
+ * return: on success 1
+ * on error -1 us retured and erro is not approprately
+ */
+
+int _putchar(char c)
+
+{
+        return (write(1,&c,1));
+}

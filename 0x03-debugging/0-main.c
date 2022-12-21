ab251225ba50e35f9d272424476e8cb0496c1299
vazttech <michaelukandu30@gmail.com>
2022-12-21 02:13:52 -0800

    0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,18 +4,11 @@
  *
  * Return: Always 0.
  */
-<<<<<<< HEAD
-
-#include "stdlib.h"
-#include "main.h"
-
-=======
->>>>>>> b68741c85d9aeaa50bd529b5928b4004c082929a
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 	positive_or_negative(i);
 
 	return (0);

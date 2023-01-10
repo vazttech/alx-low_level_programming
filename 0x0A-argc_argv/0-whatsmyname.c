9d0fa79c1d73021ec69b549b17daa5a89cff97bc
vazttech <michaelukandu30@gmail.com>
2023-01-10 14:14:44 -0800

    argv argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
-#include <stdio.h>             
-#include "main.h"                                                                                                                                
-/**                                                                                                                                  
- * main - Print the name of the program                                                                                              
- * @argc: Count arguments                                                                                                            
- * @argv: Arguments                                                                                                                  
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                          
-int main(int argc, char *argv[])                                                                                                     
-{                                                                                                                                    
-(void) argc;                                                                                                                         
-printf("%s\n", argv[0]);                                                                                                             
-                                                                                                                                     
-return (0);                                                                                                                          
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,26 @@
-#include <stdio.h>                                                                                                                   
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * main - print the name of the program                                                                                              
- * @argc: Count arguments                                                                                                            
- * @argv: Arguments                                                                                                                  
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                                  
-                                                                                                                                     
-int main(int argc, char *argv[])                                                                                                     
-{                                                                                                                                    
-(void) argv; /*Ignore argv*/                                                                                                         
-printf("%i\n", argc - 1);                                                                                                            
-                                                                                                                                     
-return (0);                                                                                                                          
-}
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
+	return (0);
+}

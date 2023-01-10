04a6d0abf313461a216f74f830e5c37ca3a099c4
vazttech <michaelukandu30@gmail.com>
2023-01-10 14:18:50 -0800

    argv argc

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
-#include <stdio.h>                                                                                                                   
-#include <stdlib.h>                                                                                                                  
-                                                                                                                                     
-/**                                                                                                                                  
+#include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
  * main - function                                                                                                                   
  *@argc: length of argv                                                                                                              
  *@argv: number of argument                                                                                                          
